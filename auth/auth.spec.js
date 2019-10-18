252d3f52895390de4b365b682bd3a2f6f7ffe579
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-18 14:11:37 -0700

    new working testing

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -4,19 +4,41 @@ const request = require('supertest')
 
 const authRouter = require('./auth-router')
 
+const server = require('../api/server')
 
 
-describe('POST /register', () => {
 
-    it('should return status 201', async () => {
-        const res = await request(authRouter).post('/regester')
+describe('GET /', () => {
+
+    // it('should return status 201', async () => {
+    //     const res = await request(authRouter).post('/regester')
+    //     expect(res.status).toBe(200)
+    // });
+
+    // it('should return the json', async () => {
+    //     const res = await request(authRouter).post('/register')
+    //     expect(res.type).toMatch(/json/i)
+    // });
+
+
+    it('returns status 200', async () => {
+        const res = await request(server).get('/')
+
         expect(res.status).toBe(200)
-    });
+    })
 
-    it('should return the json', async () => {
-        const res = await request(authRouter).post('/register')
+    it('returns JSON', async () => {
+        const res = await request(server).get('/')
+
         expect(res.type).toMatch(/json/i)
-    });
+    })
+
+    it('returns JSON object', async () => {
+        const res = await request(server).get('/')
+
+        expect(res.body).toEqual({message: 'Hello There'})
+    })
+
 
 
     // describe('POST /login', () => {
