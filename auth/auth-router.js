d73c9bc5fbd9b9c3db047d89e65745d2f5dbb4d6
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-18 09:05:01 -0700

    setting up token and register route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const Users = require('./auth-model')
+const blocker = require('./authenticate-middleware')
+const bcrypt = require('bcryptjs')
 
 router.post('/register', (req, res) => {
   // implement registration
+  const user = req.body
+  const hash = bcrypt.hashSync(user.password, 12)
+  user.password = hash
+
+  Users.add(user)
+    .then(person => {
+      res.status(200).json(console.log(person))
+    })
+    .catch(err => res.send({message: 'WHAT ARE YOU DOING!?'}))
+
 });
 
 router.post('/login', (req, res) => {
